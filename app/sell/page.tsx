12c0d96d58f0ce9307407ef3c08a40129772048b
heet-javiya <heetjaviya06@gmail.com>
2025-12-13 09:41:19 +0530

    fix:genderal changesss

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,4 +1,5 @@
 import { Faq } from "@/components/Sell/Faq";
+import { FeatureSection } from "@/components/Sell/FeatureSection";
 import { Hero } from "@/components/Sell/Hero";
 import { MarketplaceLayout } from "@/components/Sell/MarketplaceLayout";
 import { Steps } from "@/components/Sell/Steps";
@@ -8,6 +9,7 @@ const SellPage = () => {
 	return (
 		<>
 			<Hero />
+			<FeatureSection />
 			<Steps />
 			<Testimonials />
 			<MarketplaceLayout />
